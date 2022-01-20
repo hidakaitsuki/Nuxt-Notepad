7fd13f987817a113545cd79078234a388b836af6
hidakaitsuki <itsuki.hidaka@rakus-partners.co.jp>
2022-01-20 15:17:49 +0900

    フッター設定

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,5 +1,7 @@
 <template>
-  <div class="h-20 bg-white opacity-8">footer</div>
+  <div class="h-20 bg-white opacity-8 text-center">
+    <img src="../static/footer.png" class="inline-block w-20 pt-2" />
+  </div>
 </template>
 
 <script>
